c9a53435053458a870a01bdcba8bc9ec78f5ca9e
Yalamarthi Rajesh <rajeshchowdary3230@gmail.com>
2018-10-30 16:16:46 +0530

    Update SPFXTripAssessment/src/webparts/tripWebPart/TripWebPartWebPart.ts
    30 oct 2018

--- SPFXTripAssessment/src/webparts/tripWebPart/TripWebPartWebPart.ts
+++ SPFXTripAssessment/src/webparts/tripWebPart/TripWebPartWebPart.ts
@@ -24,67 +24,45 @@ export interface ITripWebPartWebPartProps {
       var updateId;
       var chartdata = [];
       var locationArray = [];
+      var allLocations = new Array();
+      var VoterCount = new Array();
 export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPartWebPartProps> {
 
   public render(): void {
     let cssURL = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
     let fontUrl = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
-   // let chartJs = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js";
     SPComponentLoader.loadCss(cssURL);
     SPComponentLoader.loadCss(fontUrl);
-  //  SPComponentLoader.loadScript(chartJs);
     this.domElement.innerHTML = `
-      <!--<div class="${ styles.tripWebPart }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-             
-              <a href="https://aka.ms/spfx" class="btn btn-primary">
-                <span class="${ styles.label }">Learn more</span>
-              </a>
-              <button type="button" class="btn btn-success"><i class="fa fa-thumbs-up fa-2x"></i></button>
-            </div>
-          </div>
-        </div>
-      </div>-->
-
-      <input type="hidden" id="updateValue">
+     <input type="hidden" id="updateValue">
       <div class="container col-md-12">
         <div class="row">
           <div class="container text-center col-md-12">
             <h1 style="padding-bottom: 5%;">Trip Spots/Location</h1>      
               <ul class="list-inline" id="locations">
-              </ul>
-              
+              </ul>              
               <button type="button" class="btn btn-success submission" style="margin-top: 5%;">Submit</button>
           </div>
         <div>
       </div>
 
-      <div col-md-12>
+      <div id="PieChartDiv">
             <canvas id="pieChart"></canvas>
       </div>
-
       `;
       var canvas = <HTMLCanvasElement> document.getElementById("pieChart");
       var cntxt = canvas.getContext("2d"); 
-      var Absourl = this.context.pageContext.web.absoluteUrl;
-      var allLocations = new Array();
-      var VoterCount = new Array()
+      var Absourl = this.context.pageContext.web.absoluteUrl;    
       
       $(document).ready(function()
       {
         GetUserDetails();
-        PopulateTripLocation();
-       
+        PopulateTripLocation();       
         allItemsCount();
         isVotedBefore();
+
         /****************get current user******************/
         function GetUserDetails() {  
-         // alert("current user get");
           var url = Absourl + "/_api/web/currentuser";  
           $.ajax({  
               url: url,  
@@ -120,7 +98,6 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
             updateId = data.d.results[0].ID;
             selectedTrip = data.d.results[0].LocationId;
             $("#updateValue").attr("value", data.d.results[0].Id);
-        //    alert("if voted selected trip :"+selectedTrip);
             $(".btn-primary").attr("disabled","disabled");
             $(".dscicon").removeClass("fa-thumbs-up");
             $(".dscicon").addClass("fa-thumbs-down");
@@ -136,7 +113,6 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
         });
 
         /**********************************************/
-       // alert("VoterCount "+VoterCount);
         var myPieChart = new Chart(cntxt, {
           type: 'pie',
           data: {
@@ -198,7 +174,7 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
         call.done(function (data, textStatus, jqXHR) {          
             $('#locations li').remove();
             var orderedList = $('#locations');
-            allLocations = data.d.results;
+            allLocations = data.d.results; 
             var counter = 10;
             $.each(data.d.results, function (idx, elem) {
                 orderedList.append("<div class='col-md-3'><div class='col-md-12' style='margin-bottom: 7%; font-weight:bold;'>"+elem.Title+"</div><div class='col-md-12'><li><button type='button' class='btn btn-primary' id='" + elem.ID + "'><i class='fa fa-thumbs-up fa-2x dscicon' id='icon"+ elem.ID +"'></button></li></div></div>");
@@ -230,8 +206,7 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
               $(".dscicon").addClass("fa-thumbs-down");    
               $("#"+selectedTrip).removeAttr("disabled"); 
               $("#icon"+selectedTrip).removeClass("fa-thumbs-down"); 
-              $("#icon"+selectedTrip).addClass("fa-thumbs-up");     
-                
+              $("#icon"+selectedTrip).addClass("fa-thumbs-up");                     
             }
             else
             {
@@ -263,10 +238,8 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
           listsObjects.value.forEach(listObject => {
           
             updateId =   listObject.ID;
-            selectedTrip = listObject.LocationId; //     <option value="${listObject.ID}">${listObject.Category}</option>
-                   
-          });
-        //  isVoted = 1;        
+            selectedTrip = listObject.LocationId; //     <option value="${listObject.ID}">${listObject.Category}</option>                   
+          });        
         }
         else
         {
@@ -308,7 +281,6 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
       this.domElement.querySelector('#listdata').innerHTML = "Sorry this does not work in local workbench";
     } 
     else{
-
       /*************Check Voter*************************/
       this.context.spHttpClient.get(
         this.context.pageContext.web.absoluteUrl+`/_api/Web/Lists/getByTitle('DscVoting')/Items?$select=Title,ID,LocationId&$filter=(Author eq '`+currentUserId+`')`,
@@ -317,15 +289,12 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
       .then((response: SPHttpClientResponse) => {
         response.json().then((listsObjects: any) => {
           
-            listsObjects.value.forEach(listObject => {
-            
+            listsObjects.value.forEach(listObject => {            
               updateId =   listObject.ID;
-              selectedTrip = listObject.LocationId; //     <option value="${listObject.ID}">${listObject.Category}</option>
-                     
+              selectedTrip = listObject.LocationId; 
             });
         });
       });
-      //alert("isVotterVoted :"+isVoted);
       if(selectedTrip == null || selectedTrip == '' || selectedTrip == isEmpty)
       {
         alert("Please Select Location");
@@ -334,18 +303,89 @@ export default class TripWebPartWebPart extends BaseClientSideWebPart<ITripWebPa
       {
         if(updateId == '' || updateId == null || updateId == isEmpty)
         {
-            alert("if :"+selectedTrip);
-            pnp.sp.web.lists.getByTitle('DscVoting').items.add({ Title :'Inserted',LocationId : selectedTrip}); 
-            alert("Inserted Successfully!!!");
+             pnp.sp.web.lists.getByTitle('DscVoting').items.add({ Title :'Inserted',LocationId : selectedTrip})
+            .then(() =>
+            {
+            //   var call = jQuery.ajax({
+            //     url: this.context.pageContext.web.absoluteUrl + "/_api/Web/Lists/getByTitle('DscVoting')/Items?$select=Title,ID,LocationId&$filter=(Author eq '"+currentUserId+"')",
+            //     type: "GET",
+            //     dataType: "json",
+            //     async: false,  
+            //     headers: {
+            //         Accept: "application/json; odata=verbose",
+            //         "Content-Type": "application/json;odata=verbose"
+            //     }
+            // });
+            //   call.done(function (data, textStatus, jqXHR) {          
+            //     isVoted = data.d.results.length;
+                
+                
+            //   });
+            //   call.fail(function (jqXHR, textStatus, errorThrown) {
+            //     var response = JSON.parse(jqXHR.responseText);
+            //     var message = response ? response.error.message.value : textStatus;
+            //     alert("error getting usvoted: " + message);
+            //   });
+              alert("Inserted Successfully!!!");
+              window.location.reload();
+             // this.redrawChart();
+            });             
         }
         else
         {        
             alert("record to update :"+updateId);
-            pnp.sp.web.lists.getByTitle('DscVoting').items.getById(parseInt(updateId)).update({Title :'Updated',LocationId : selectedTrip});
-            alert("Updated Successfully!!!");
-          
+            pnp.sp.web.lists.getByTitle('DscVoting').items.getById(parseInt(updateId)).update({Title :'Updated',LocationId : selectedTrip})
+            .then(() => { 
+              alert("Updated Successfully!!!");
+              this.redrawChart();
+             });             
         }
       }
     }
+  }
+
+  redrawChart()
+  {
+    var call = jQuery.ajax({
+      url: this.context.pageContext.web.absoluteUrl + "/_api/Web/Lists/getByTitle('DscVoting')/Items?$select=Title,ID,LocationId",
+        type: "GET",
+        dataType: "json",
+        async: false,  
+        headers: {
+            Accept: "application/json; odata=verbose",
+            "Content-Type": "application/json;odata=verbose"
+        }
+      });
+      call.done(function (data, textStatus, jqXHR) {  
+        VoterCount = [];
+          for(var i=0;i<allLocations.length;i++)
+          {
+            VoterCount.push(data.d.results.filter(value => value.LocationId === allLocations[i].Id).length);
+          }
+          $("#PieChartDiv").empty();   //<canvas id="pieChart"></canvas>
+          var PieChartDiv = $('#PieChartDiv');
+          PieChartDiv.append(" <canvas id='pieChart'></canvas>");
+          var canvas = <HTMLCanvasElement> document.getElementById("pieChart");
+          var cntxt = canvas.getContext("2d"); 
+          var myPieChart = new Chart(cntxt, {
+            type: 'pie',
+            data: {
+              labels: locationArray,
+                datasets: [
+                    {
+                        labels: locationArray,
+                        data: VoterCount,
+                        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#4D5360"],   //"#4D5360"
+                        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5"]
+                    }
+                ]
+            },
+            options: {
+                responsive: true
+            }
+        });
+      });
+      call.fail(function (jqXHR, textStatus, errorThrown) {
+      });
   }
 }
